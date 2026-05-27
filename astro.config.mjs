43709d2eb5d520523df2fce1d1e358fcd03d7aac
Anastasiia Berest <anastasiiaberest@gmail.com>
2026-05-27 15:51:00 +0300

    template

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,24 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
-
+import tailwindcss from "@tailwindcss/vite";
+import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from './src/locales';
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://fyrre-seven.vercel.app/',
 	integrations: [mdx(), sitemap()],
+	image: { domains: ["cdn.sanity.io"], },
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	i18n: {
+		defaultLocale: DEFAULT_LOCALE_SETTING,
+		locales: Object.keys(LOCALES_SETTING),
+		routing: {
+			prefixDefaultLocale: true,
+			//redirectToDefaultLocale: false,
+		},
+	},
 	fonts: [
 		{
 			provider: fontProviders.local(),
